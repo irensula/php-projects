a6254249534fcd472ab8db76aafad8113cb0b977
irensula <irinawebdev@gmail.com>
2024-02-08 16:00:58 +0200

    Add files via upload

---
+++ PHP connection SQL/includes/dbh.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+$dsn = "mysql:host=localhost;dbname=myfirstdatabase";
+$dbusername = "root";
+$dbpassword = "";
+
+try {
+    // pdo - PHP Database Object
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOExseption $e) {
+    echo "Connection failed: " . $e->getMessages(); 
+}
