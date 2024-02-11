83c164bdfa45d7ee8d244fb76d7082d50a96cb0b
irensula <irinawebdev@gmail.com>
2024-02-11 22:40:11 +0200

    Add files via upload

--- PHP connection SQL/index.php
+++ PHP connection SQL/index.php
@@ -35,5 +35,11 @@
         <button>Delete</button>
     </form>
 
+    <form action="search.php" method="post">
+        <label for="search">Search for user:</label>
+        <input id="search" type="text" name="usersearch" placeholder="Search...">
+        <button>Search</button>
+    </form>
+
 </body>
 </html>
