98a9312b3dc5954dccff21bbd8cefc3416050690
irensula <irinawebdev@gmail.com>
2024-03-14 23:26:18 +0200

    Update dbh.inc.php

--- Hash/includes/dbh.inc.php
+++ Hash/includes/dbh.inc.php
@@ -9,5 +9,5 @@ try {
     $pdo = new PDO($dsn, $dbusername, $dbpassword);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessages(); 
+    echo "Connection failed: " . $e->getMessage(); 
 }
